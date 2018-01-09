611b4bff15740d793bf30903351840f8cdc36c5f
Loïc Hoguin <essen@ninenines.eu>
2018-01-09 17:11:27 +0100

    Make the NIF load/unload/upgrade functions static
    Without this there is some conflict between NIFs on OSX
    where the first NIF loaded will have its functions called
    for all NIFs. I am not sure why exactly but this fixes it.

--- c_src/lz4.c
+++ c_src/lz4.c
@@ -13,15 +13,12 @@
 // info@rabbitmq.com.
 
 #include "lz4_erlang.h"
+#include <stdio.h>
 
 NIF_ATOMS(NIF_ATOM_DECL)
 NIF_RESOURCES(NIF_RES_DECL)
 
-int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info);
-int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info);
-void unload(ErlNifEnv* env, void* priv_data);
-
-int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+static int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 {
     NIF_ATOMS(NIF_ATOM_INIT)
     NIF_RESOURCES(NIF_RES_INIT)
@@ -29,14 +26,14 @@ int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
     return 0;
 }
 
-int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info)
+static int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info)
 {
     *priv_data = *old_priv_data;
 
     return 0;
 }
 
-void unload(ErlNifEnv* env, void* priv_data)
+static void unload(ErlNifEnv* env, void* priv_data)
 {
 }
 
